67c315245719529b192b51e7fd342c6216beee6d
github username <github email address>
2019-12-29 13:34:12 -0800

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,37 +1,95 @@
+require 'pry'
 class CashRegister
 
-    attr_accessor :discount, :total, :all, :last_price
-    # @@all =[]
+    attr_accessor :total, :discount, :items, :last_item
 
-    def initialize(emp_disc = 0)
-        @total = 0
-        @discount = emp_disc
-        @all = []
-        @last_price = 0
+    def initialize(employee_disc = 0)
+        self.total = 0
+        self.discount = employee_disc
+        self.items = []
     end
 
-    def add_item(title, price, quant = 1)
-        quant.times do 
-        @all << title
+    def add_item(item, price, quantity = 1)
+        self.total += price * quantity
+        self.last_item = price * quantity
+        quantity.times do
+            items << item
         end
-        @last_price = price * quant
-        @total += price * quant
+        # self.void_last_transaction_amount = price * quantity
     end
 
     def apply_discount
-        if @discount == 0
-           return "There is no discount to apply."
+        if discount != 0
+            discounted_total = self.total = total - discount * 10
+            return "After the discount, the total comes to $#{discounted_total}."
+        else
+            return "There is no discount to apply."
         end
-        @total -= (@discount.to_f/100)*(@total)
-        return "After the discount, the total comes to $#{@total.to_i}."
-    end
-
-    def items
-        @all
     end
 
     def void_last_transaction
-        @total -= @last_price
+        self.total -= self.last_item
     end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class CashRegister
+
+#     attr_accessor :discount, :total, :all, :last_price
+#     # @@all =[]
+
+#     def initialize(employee_disc = 0)
+#         @total = 0
+#         @discount = employee_disc
+#         @all = []
+#         @last_price = 0
+#     end
+
+#     def add_item(title, price, quant = 1)
+#         quant.times do 
+#         @all << title
+#         end
+#         @last_price = price * quant
+#         @total += price * quant
+#     end
+
+#     def apply_discount
+#         if @discount == 0
+#            return "There is no discount to apply."
+#         end
+#         @total -= (@discount.to_f/100)*(@total)
+        
+#         return "After the discount, the total comes to $#{@total.to_i}."
+#     end
+
+#     def items
+#         @all
+#     end
+
+#     def void_last_transaction
+#         @total -= @last_price
+#     end
+
+# end
