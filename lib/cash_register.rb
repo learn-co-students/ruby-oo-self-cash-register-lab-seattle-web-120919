5621f5d9d83f862d786bd22210cebd8d5f5b6fea
github username <github email address>
2020-01-22 13:51:40 -0800

    Redone

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,95 +1,54 @@
-require 'pry'
+require "pry"
+
 class CashRegister
+  attr_accessor :total, :discount, :price, :cart, :last_item, :last_quantity
 
-    attr_accessor :total, :discount, :items, :last_item
+  def initialize(discount = 0)
+    self.total = 0
+    self.discount = discount
+    self.cart = []
+  end
 
-    def initialize(employee_disc = 0)
-        self.total = 0
-        self.discount = employee_disc
-        self.items = []
+  # adds the given item(s) price to the total
+  # need to count through the qunaity in order to place multiples of
+  # the same item in the cart
+  def add_item(item, price, quantity = 1)
+    self.total += price * quantity
+    self.last_item = price
+    self.last_quantity = quantity
+
+    counter = 0
+    while counter < quantity
+      self.cart << item
+      counter += 1
     end
+    self.cart
+  end
 
-    def add_item(item, price, quantity = 1)
-        self.total += price * quantity
-        self.last_item = price * quantity
-        quantity.times do
-            items << item
-        end
-        # self.void_last_transaction_amount = price * quantity
+  # apply the discount(20) to the total of the purchases
+  def apply_discount
+    if self.discount > 0
+      self.total = @total - @total * @discount / 100
+      return "After the discount, the total comes to $#{@total}."
+    else
+      return "There is no discount to apply."
     end
+  end
 
-    def apply_discount
-        if discount != 0
-            discounted_total = self.total = total - discount * 10
-            return "After the discount, the total comes to $#{discounted_total}."
-        else
-            return "There is no discount to apply."
-        end
+  def items
+    self.cart
+  end
+
+  # in order for us to use remove the last item price we are going to need to referance
+  # add_item and access its price. Set a new instance variable (last_time) so our void method can access it.
+  # set the last_item to the price of the item then set a new variable
+  # we will need to multiply the last transactions quanity as well.
+  def void_last_transaction
+    self.total -= self.last_item * self.last_quantity
+    self.cart.pop
+    if self.cart.length <= 0
+      self.total = 0.0
     end
-
-    def void_last_transaction
-        self.total -= self.last_item
-    end
-
+    # binding.pry
+  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class CashRegister
-
-#     attr_accessor :discount, :total, :all, :last_price
-#     # @@all =[]
-
-#     def initialize(employee_disc = 0)
-#         @total = 0
-#         @discount = employee_disc
-#         @all = []
-#         @last_price = 0
-#     end
-
-#     def add_item(title, price, quant = 1)
-#         quant.times do 
-#         @all << title
-#         end
-#         @last_price = price * quant
-#         @total += price * quant
-#     end
-
-#     def apply_discount
-#         if @discount == 0
-#            return "There is no discount to apply."
-#         end
-#         @total -= (@discount.to_f/100)*(@total)
-        
-#         return "After the discount, the total comes to $#{@total.to_i}."
-#     end
-
-#     def items
-#         @all
-#     end
-
-#     def void_last_transaction
-#         @total -= @last_price
-#     end
-
-# end
